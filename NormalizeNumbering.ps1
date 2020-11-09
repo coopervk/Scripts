d6fbc4c74f8b6db8b988c70b389fe8aff526aa9d
Cooper <coopervk@gmail.com>
2020-11-09 13:28:52 -0600

    Add basic powershell script for normalizing numbering in files in order to watch course materials more easily

---
+++ NormalizeNumbering.ps1
@@ -0,0 +1,23 @@
+﻿# Rename file naming for very basic files
+#  1.mp4,  2.mp4, ...,  9.mp4, 10.mp4... to
+# 01.mp4, 02.mp4, ..., 09.mp4, 10.mp4
+
+# If you file names are more complex, add the part which leads up to the number
+# between the ^ and \d, then the part after the number between \d and \.
+
+# Currently only supports files numbered [0, 100)
+
+# Made for the purpose of being able to go "mpv *" in a directory with lots of
+# videos that one would want to watch back to back.
+
+$cwd = Get-Location | Select -ExpandProperty Path
+
+Get-ChildItem "$cwd" | 
+Foreach-Object {
+    $Path = $_.FullName
+    $Name = $_.Name
+    if ( $Name -match '^\d\.' ) {
+        $NewName = "0" + "$Name"
+        Rename-Item -Path $Path -NewName $NewName
+    }
+}
